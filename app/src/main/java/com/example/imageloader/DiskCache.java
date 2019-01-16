970b3171072ba94e5ea130d645e4d37a6ebd50e7
jsyjst <y8588130@163.com>
2019-01-16 17:05:10 +0800

    增加sd卡缓存功能

---
+++ app/src/main/java/com/example/imageloader/DiskCache.java
@@ -0,0 +1,50 @@
+package com.example.imageloader;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * <pre>
+ *     author : 残渊
+ *     time   : 2019/01/16
+ *     desc   : sd卡缓存类
+ * </pre>
+ */
+
+
+public class DiskCache implements IImageCache {
+    private static String cacheDir ;
+
+    public DiskCache(Context context){
+        cacheDir = context.getExternalFilesDir("").getAbsolutePath();
+    }
+
+    @Override
+    public Bitmap get(String url) {
+        return BitmapFactory.decodeFile(cacheDir + url);
+    }
+
+    @Override
+    public void put(String url, Bitmap bmp) {
+        FileOutputStream fileOutputStream = null;
+        try{
+            fileOutputStream = new FileOutputStream(cacheDir + url);
+            bmp.compress(Bitmap.CompressFormat.PNG,100,fileOutputStream);
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } finally  {
+            if(fileOutputStream != null){
+                try {
+                    fileOutputStream.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
